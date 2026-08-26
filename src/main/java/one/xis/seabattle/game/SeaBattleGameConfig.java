714cfdf14823c612ab291635784707e3c96514af
bernd9 <bernd@peltzer-bartels.de>
2026-08-27 01:38:29 +0200

    Increase boat engine speeds

--- src/main/java/one/xis/seabattle/game/SeaBattleGameConfig.java
+++ src/main/java/one/xis/seabattle/game/SeaBattleGameConfig.java
@@ -1,12 +1,12 @@
 package one.xis.seabattle.game;
 
-record SeaBattleGameConfig(VehicleScale vehicleScale) {
+record SeaBattleGameConfig(VehicleScale vehicleScale, double[] engineSpeeds) {
 
     static final double TORPEDO_BOAT_SCALE = 3.0;
     static final double SCOUT_PLANE_SCALE = 1.5;
 
     static SeaBattleGameConfig current() {
-        return new SeaBattleGameConfig(new VehicleScale(TORPEDO_BOAT_SCALE, SCOUT_PLANE_SCALE));
+        return new SeaBattleGameConfig(new VehicleScale(TORPEDO_BOAT_SCALE, SCOUT_PLANE_SCALE), EngineOrders.speeds());
     }
 
     record VehicleScale(double torpedoBoat, double scoutPlane) {
